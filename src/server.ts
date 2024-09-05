df518f6706ee7e961330e8211721522871818d0c
Ray Silva <silvaray355@gmail.com>
2024-09-05 18:00:46 -0300

    Refatorando

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import  express  from "express";
+import express  from "express";
 import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+
 const port = process.env.PORT;
 const api = express();
+
 api.use(express.json());
 api.use(cors());
 
 api.listen(port, () => {
-    console.log(`Server online On Port ${port}`)
+    console.log(`Server online on port: ${port}`)
 });
