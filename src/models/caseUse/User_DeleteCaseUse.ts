9d5151fe34ec33e8e7ee35261d744f5993b609b3
IsraelLazaro <israel.lazaro@academico.ifpb.edu.br>
2024-09-27 10:48:53 -0300

    Ajuste final no delete User

--- src/models/caseUse/User_DeleteCaseUse.ts
+++ src/models/caseUse/User_DeleteCaseUse.ts
@@ -1,4 +1,6 @@
 import User from "../entities/user";
+import PetsModel from "../entities/pets";
+import { RelationsPet } from "../entities/relations";
 
 export class DeleteUserCaseUse {    
     async execute(id: string): Promise<void> {
@@ -7,6 +9,24 @@ export class DeleteUserCaseUse {
             throw new Error('Usuário não encontrado no sistema!');
         };
         try {
+            const userPets = user.pets; 
+            if(userPets && userPets.length>0){
+                for (const pet of userPets) {
+                    const petInList = await PetsModel.findById(pet).exec();
+                    if(petInList){                        
+                        const listRelation = petInList.relations;        
+                        if (listRelation && listRelation.length > 0) {
+                            for (const relation of listRelation) {
+                                const relationDocument = await RelationsPet.findById(relation).exec();                 
+                                if (relationDocument) {
+                                    await relationDocument.deleteOne();
+                                };
+                            };
+                        };
+                        await petInList.deleteOne();
+                    };
+                };
+            };
             await user.deleteOne();
         } catch (error) {
             console.error("Erro ao deletar usuário:", error);
